b5d3adfed8614834589e78339be950cbde0e10e5
Sampath S <sampathsr88@gmail.com>
2024-08-30 12:56:23 +0530

    Intial project code added

---
+++ PPRS/Form1.cs
@@ -0,0 +1,25 @@
+namespace PPRS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
+    }
+}
